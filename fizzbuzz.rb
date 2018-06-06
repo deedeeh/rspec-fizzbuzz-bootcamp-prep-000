6155039bc2fc5b0f9ee6463dfb6abf256ad53a48
Dina Hafez <dinahafez.8@gmail.com>
2018-06-06 19:01:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -13,11 +13,11 @@
 
 def fizzbuzz(input)
   if input % 3 && input % 5 == 0 
-    "Fizz"
-  elsif input % 3 == 0
-    "Buzz"
-  elsif input % 5 == 0
     "FizzBuzz"
+  elsif input % 3 == 0
+    "Fizz"
+  elsif input % 5 == 0
+    "Buzz"
   else 
     print "nil"
   end
