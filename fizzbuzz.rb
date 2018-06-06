4c7cc31f182a441470d003361e43e43f14c94c62
Dina Hafez <dinahafez.8@gmail.com>
2018-06-06 19:07:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,4 @@
 
-# def fizzbuzz(input)
-#   if input % 3 == 0
-#     "Fizz"
-#   elsif input % 5 == 0
-#     "Buzz"
-#   elsif input % 3 && input % 5 == 0 
-#     "FizzBuzz"
-#   else 
-#     print "nil"
-#   end
-# end
-
 def fizzbuzz(input)
   if input % 3 == 0 && input % 5 == 0 
     "FizzBuzz"
